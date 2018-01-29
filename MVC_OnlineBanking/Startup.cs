c527c63f4b98a3f791ad95058fa92a6257a3a747
DStanarevic <dejanstanarevic@gmai.com>
2018-01-28 20:47:17 -0500

    Initial commit

---
+++ MVC_OnlineBanking/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_OnlineBanking.Startup))]
+namespace MVC_OnlineBanking
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
